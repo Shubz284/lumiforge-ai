29592013116d01459aded23aab7ecb1392689687
Shubham Ghorpade <shubhamghorpade284@gmail.com>
2026-07-31 22:52:42 +0530

    fix the Payment Verification

--- apps/backend/src/routes/payment.routes.ts
+++ apps/backend/src/routes/payment.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { requireAuth } from "../middlware/requireAuth";
-import { createRazorpayOrder, creditsWebhookHandler, transactionHistory } from "../controller/payment.controller";
-import { verifyPaymentSignature } from "../lib/verifyPaymentSignature";
+import { createRazorpayOrder, transactionHistory, verifyPayment } from "../controller/payment.controller";
 
 const router = Router();
 
 router.post("/payments/create-order", requireAuth, createRazorpayOrder);
-router.post("/payments/verify", requireAuth, verifyPaymentSignature);
+router.post("/payments/verify", requireAuth, verifyPayment);
 router.get("/payments/transactions", requireAuth, transactionHistory)
 
 export default router
